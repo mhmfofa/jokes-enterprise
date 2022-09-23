3cee1e368179c6644029c07ba1ddb7eeb7db5809
m.molayemi <hassan@molayemi.com>
2022-09-23 19:01:58 +0330

    fix/failed-test-fixed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'jokes-enterprise! haha'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('jokes-enterprise');
+    expect(app.title).toEqual('jokes-enterprise! haha');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('jokes-enterprise app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('jokes-enterprise! haha app is running!');
   });
 });
